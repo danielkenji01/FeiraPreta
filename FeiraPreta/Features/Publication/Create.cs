6e49557936ef04d0698bef5b98671ac623ded827
danielkenji01 <daniel_kenji_1@hotmail.com>
2018-03-01 21:51:19 -0300

    metodos de publication

---
+++ FeiraPreta/Features/Publication/Create.cs
@@ -0,0 +1,52 @@
+﻿using FeiraPreta.Infrastructure;
+using MediatR;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeiraPreta.Features.Publication
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public string Link { get; set; }
+        }
+
+        public class CommandHandler : IAsyncRequestHandler<Command>
+        {
+            private readonly Db db;
+
+            public CommandHandler(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task Handle(Command message)
+            {
+                var shortcode = message.Link.Substring(28);
+
+                var media = "https://api.instagram.com/v1/media/" + shortcode + "D?access_token=4262635600.1677ed0.c1c01fc89e8242fe803084b010db36dc";
+
+                var client = new RestClient(media);
+
+                var request = new RestRequest(Method.GET);
+
+                var response = client.Execute(request);
+
+                Console.WriteLine(response);
+
+                var publication = new Domain.Publication
+                {
+                    Link = message.Link,
+                };
+
+                db.Publication.Add(publication);
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ FeiraPreta/Features/Publication/List.cs
@@ -0,0 +1,61 @@
+﻿using FeiraPreta.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeiraPreta.Features.Publication
+{
+    public class List
+    {
+        public class Query : IRequest<IList<Result>>
+        {
+            public Query()
+            {
+
+            }
+        }
+
+        public class Result
+        {
+            public Guid Id { get; set; }
+
+            public string ImageLowResolution { get; set; }
+
+            public string ImageThumbnail { get; set; }
+
+            public string ImageStandardResolution { get; set; }
+
+            public bool IsHighlight { get; set; }
+
+            public string Subtitle { get; set; }
+        }
+
+        public class QueryHandler : IAsyncRequestHandler<Query, IList<Result>>
+        {
+            private readonly Db db;
+
+            public QueryHandler(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task<IList<Result>> Handle(Query message)
+            {
+                return await db.Publication
+                               .Where(p => p.IsHighlight)
+                               .Select(p => new Result
+                               {
+                                   Id = p.Id,
+                                   ImageLowResolution = p.ImageLowResolution,
+                                   ImageStandardResolution = p.ImageStandardResolution,
+                                   ImageThumbnail = p.ImageThumbnail,
+                                   IsHighlight = p.IsHighlight,
+                                   Subtitle = p.Subtitle
+                               }).ToListAsync();
+            }
+        }
+    }
+}
