558460ab911f260194cbff51299ab6031db4e9d6
danielkenji01 <daniel_kenji_1@hotmail.com>
2018-03-02 21:28:42 -0300

    consumir o json do instagram

--- FeiraPreta/Features/Publication/Create.cs
+++ FeiraPreta/Features/Publication/Create.cs
@@ -1,9 +1,12 @@
 ﻿using FeiraPreta.Infrastructure;
 using MediatR;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace FeiraPreta.Features.Publication
@@ -26,26 +29,45 @@ namespace FeiraPreta.Features.Publication
 
             public async Task Handle(Command message)
             {
-                var shortcode = message.Link.Substring(28);
+                string shortcode = message.Link.Substring(28, 11);
 
-                var media = "https://api.instagram.com/v1/media/" + shortcode + "D?access_token=4262635600.1677ed0.c1c01fc89e8242fe803084b010db36dc";
+                string url = "https://api.instagram.com/v1/media/shortcode/" + shortcode + "?access_token=7207542169.480fb87.1cc924b10c4b43a5915543675bd5f736";
 
-                var client = new RestClient(media);
+                WebResponse response = processWebRequest(url);
 
-                var request = new RestRequest(Method.GET);
+                Domain.Publication publication = new Domain.Publication();
 
-                var response = client.Execute(request);
-
-                Console.WriteLine(response);
-
-                var publication = new Domain.Publication
+                using (var sr = new System.IO.StreamReader(response.GetResponseStream()))
                 {
-                    Link = message.Link,
+                    // var publication = JsonConvert.ToString();
+
+                    var json = JObject.Parse(await sr.ReadToEndAsync());
+
+                    publication.ImageLowResolution = json["data"]["images"]["low_resolution"]["url"].ToString();
+                    publication.ImageStandardResolution = json["data"]["images"]["standard_resolution"]["url"].ToString();
+                    publication.ImageThumbnail = json["data"]["images"]["thumbnail"]["url"].ToString();
+                    publication.PersonId = new Guid("3783B665-C040-40ED-89FB-FD0B83810201");
+                    publication.CreatedDate = DateTime.Now;
+                    publication.IsHighlight = true;
+                    publication.CreatedDateInstagram = DateTime.Now;
+                    publication.Subtitle = json["data"]["caption"]["text"].ToString();
+                    publication.Link = message.Link;
+
+                    db.Publication.Add(publication);
                 };
 
-                db.Publication.Add(publication);
-
                 await db.SaveChangesAsync();
+            }
+
+            private WebResponse processWebRequest(string url)
+            {
+                WebRequest request;
+                WebResponse response;
+
+                request = WebRequest.Create(url);
+                response = request.GetResponse();
+
+                return response;
             }
         }
     }
