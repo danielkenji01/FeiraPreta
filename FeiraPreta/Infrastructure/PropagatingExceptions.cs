052a502517107f180dfd397308f3ea83fad0e97c
Daniel Kenji <daniel_kenji_1@hotmail.com>
2018-03-04 11:09:30 -0300

    classe para tratar os HttpStatusCode

---
+++ FeiraPreta/Infrastructure/PropagatingExceptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SPlay.Infraestructure
+{
+    public class HttpException : Exception
+    {
+        public int StatusCode { get; set; }
+
+        public dynamic Body { get; set; }
+
+        public HttpException(int statusCode)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        public HttpException(int statusCode, dynamic body)
+        {
+            this.StatusCode = statusCode;
+            this.Body = body;
+        }
+    }
+
+    /// <summary>
+    /// 404 status code, resource not found
+    /// </summary>
+    public class NotFoundException : HttpException
+    {
+        public NotFoundException() : base(404)
+        {
+        }
+    }
+
+    /// <summary>
+    /// 409 status code, resource already exists
+    /// </summary>
+    public class ConflictException : HttpException
+    {
+        public ConflictException() : base(409)
+        {
+        }
+    }
+}
