f0fe418bd18287477d47502c932a13e13af084e8
danielkenji01 <daniel_kenji_1@hotmail.com>
2018-03-06 21:36:29 -0300

    tabelas adicionadas

--- FeiraPreta/Infrastructure/Db.cs
+++ FeiraPreta/Infrastructure/Db.cs
@@ -19,18 +19,28 @@ namespace FeiraPreta.Infrastructure
 
         public DbSet<Publication> Publication { get; set; }
 
+        public DbSet<Tag> Tag { get; set; }
+
+        public DbSet<Publication_Tag> Publication_Tag { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<EventScore>().ToTable("EventScore");
             modelBuilder.Entity<Person>().ToTable("Person");
             modelBuilder.Entity<Publication>().ToTable("Publication");
+            modelBuilder.Entity<Tag>().ToTable("Tag");
+            modelBuilder.Entity<Publication_Tag>().ToTable("Publication_Tag");
 
             modelBuilder.Entity<Person>().HasMany(p => p.Publications).WithOne(p => p.Person).HasForeignKey(p => p.PersonId);
             modelBuilder.Entity<Person>().Property(p => p.CreatedDate).ValueGeneratedOnAdd();
             modelBuilder.Entity<Person>().Property(p => p.UpdatedDate).ValueGeneratedOnAddOrUpdate();
-
+            
             modelBuilder.Entity<Publication>().Property(p => p.CreatedDate).ValueGeneratedOnAdd();
             modelBuilder.Entity<Publication>().Property(p => p.UpdatedDate).ValueGeneratedOnAddOrUpdate();
+
+            modelBuilder.Entity<Publication_Tag>().HasKey(p => new { p.PublicationId, p.TagId }).ForSqlServerIsClustered(true);
+            modelBuilder.Entity<Publication_Tag>().HasOne(p => p.Publication).WithMany(p => p.Publication_Tags).HasForeignKey(p => p.PublicationId);
+            modelBuilder.Entity<Publication_Tag>().HasOne(p => p.Tag).WithMany(p => p.Publication_Tags).HasForeignKey(p => p.TagId);
 
             base.OnModelCreating(modelBuilder);
         }
