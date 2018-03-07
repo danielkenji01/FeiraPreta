d5629b5f370ba23c36b29286dc9c7248f19a251d
danielkenji01 <daniel_kenji_1@hotmail.com>
2018-03-06 21:35:57 -0300

    delete person e publication

---
+++ FeiraPreta/Features/Person/Delete.cs
@@ -0,0 +1,40 @@
+﻿using FeiraPreta.Infraestructure;
+using FeiraPreta.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeiraPreta.Features.Person
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IAsyncRequestHandler<Command>
+        {
+            private readonly Db db;
+
+            public Handler(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task Handle(Command message)
+            {
+                var person = await db.Person.SingleOrDefaultAsync(p => p.Id == message.Id);
+
+                if (person == null || person.DeletedDate.HasValue) throw new NotFoundException();
+
+                person.DeletedDate = DateTime.Now;
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ FeiraPreta/Features/Publication/Delete.cs
@@ -0,0 +1,40 @@
+﻿using FeiraPreta.Infraestructure;
+using FeiraPreta.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeiraPreta.Features.Publication
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IAsyncRequestHandler<Command>
+        {
+            private readonly Db db;
+
+            public Handler(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task Handle(Command message)
+            {
+                var publication = await db.Publication.SingleOrDefaultAsync(p => p.Id == message.Id);
+
+                if (publication == null || publication.DeletedDate.HasValue) throw new NotFoundException();
+
+                publication.DeletedDate = DateTime.Now;
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
