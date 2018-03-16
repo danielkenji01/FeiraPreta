ebfd6f95c0e02da4311229f598c59ffc574a253e
Daniel Kenji <daniel_kenji_1@hotmail.com>
2018-03-15 22:57:51 -0300

    mudança no nome da pasta

--- FeiraPreta/Infraestructure/ExceptionHandlerMiddleware.cs
+++ FeiraPreta/Infraestructure/ExceptionHandlerMiddleware.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FeiraPreta.Infrastructure
+namespace FeiraPreta.Infraestructure
 {
     public class ExceptionHandlerMiddleware
     {
