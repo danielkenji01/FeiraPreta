74609a0dbfeee33ae03bcb9e2910e1c2cae0f3ea
Daniel Kenji <daniel_kenji_1@hotmail.com>
2018-04-02 21:24:42 -0300

    correção do update de person

--- FeiraPreta/Features/Person/Update.cs
+++ FeiraPreta/Features/Person/Update.cs
@@ -32,7 +32,7 @@ namespace FeiraPreta.Features.Person
 
             public async Task Handle(Command message)
             {
-                if (message.PhoneNumber == null || message.Username == null) throw new HttpException(400, "Preencha pelo menos um dos campos");
+                if (message.PhoneNumber == null && message.Username == null) throw new HttpException(400, "Preencha pelo menos um dos campos");
 
                 var person = await db.Person.SingleOrDefaultAsync(p => p.Id == message.Id);
 
@@ -42,7 +42,7 @@ namespace FeiraPreta.Features.Person
 
                 if (message.Username != null)
                 {
-                    if (await db.Person.SingleOrDefaultAsync(p => p.UsernameInstagram == message.Username) != null) throw new HttpException(409, "Usuário já existe");
+                    if (await db.Person.Where(p => !p.DeletedDate.HasValue).SingleOrDefaultAsync(p => p.UsernameInstagram == message.Username) != null) throw new HttpException(409, "Usuário já existe");
 
                     string url = "https://api.instagram.com/v1/users/search?q=" + message.Username + "&access_token=7207542169.480fb87.1cc924b10c4b43a5915543675bd5f736";
 
@@ -53,19 +53,17 @@ namespace FeiraPreta.Features.Person
                         var json = JObject.Parse(await sr.ReadToEndAsync());
 
                         if (json["data"].Count() == 0) throw new HttpException(404, "Usuário não encontrado");
-
-                        person = new Domain.Person
-                        {
-                            ProfilePictureInstagram = json["data"][0]["profile_picture"].ToString(),
-                            FullNameInstagram = json["data"][0]["full_name"].ToString(),
-                            UsernameInstagram = json["data"][0]["username"].ToString(),
-                            CreatedDate = DateTime.Now,
-                            IdInstagram = json["data"][0]["id"].ToString()
-                        };
+                        
+                        person.ProfilePictureInstagram = json["data"][0]["profile_picture"].ToString();
+                        person.FullNameInstagram = json["data"][0]["full_name"].ToString();
+                        person.UsernameInstagram = json["data"][0]["username"].ToString();
+                        person.UpdatedDate = DateTime.Now;
+                        person.IdInstagram = json["data"][0]["id"].ToString();
                     }
+
+                    if (message.PhoneNumber == null) person.PhoneNumber = "0000-0000";
                 }
 
-                db.Person.Update(person);
                 await db.SaveChangesAsync();
             }
 
